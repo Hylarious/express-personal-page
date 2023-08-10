6e5a0e7d8b0d2a6f55042b91a4726f5aa8c80a17
Karolina Hyla <karatakarabe@gmail.com>
2023-08-11 00:07:10 +0200

    Add file form with multer

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer  = require('multer')
+const upload = multer({ dest: 'public/' })
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.urlencoded( { extended: false } ));
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
@@ -23,6 +27,21 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render('contact');
 });
+
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+  const file = req.file;
+
+  if(author && sender && title && message && file) {
+    res.render('contact', { isSent: true, fileName: file.originalname });
+
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+  
+})
 
 app.get('/info', (req, res) => {
   res.render('info');
